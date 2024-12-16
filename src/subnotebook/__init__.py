6ec4ae41635ab5fde8e041bdc66d154f75203338
David Brochart <david.brochart@gmail.com>
2024-12-16 23:09:29 +0100

    Update

---
+++ src/subnotebook/__init__.py
@@ -0,0 +1,5 @@
+from ._subnotebook import Return as Return
+from ._subnotebook import default_value as default_value
+from ._subnotebook import display_nb as display_nb
+from ._subnotebook import open_nb as open_nb
+from ._subnotebook import run_nb as run_nb
--- subnotebook/__init__.py
+++
@@ -1,1 +0,0 @@
-from .subnotebook import run_nb, open_nb, default_value, Return, display_nb
